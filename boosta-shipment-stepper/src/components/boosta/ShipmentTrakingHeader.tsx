48b876dbd5280c05f781c22fdde6d86e79b17db4
mohamedHamdy011574 <mohamedhamdydeveloper55@gmail.com>
2024-12-10 13:45:16 +0200

    handle contrast issue in some elements

--- boosta-shipment-stepper/src/components/boosta/ShipmentTrakingHeader.tsx
+++ boosta-shipment-stepper/src/components/boosta/ShipmentTrakingHeader.tsx
@@ -17,7 +17,7 @@ const ShipmentTrakingHeader = ({ shipmentData }: { shipmentData: any }) => {
 
   return (
     <div>
-      <span className="text-sm font-[600] text-gray-400">
+      <span className="text-sm font-[600] text-gray-700">
         {trans("ORDER")} #{shipmentData.TrackingNumber}
       </span>
       <div className="border-b border-b-gray-400">
